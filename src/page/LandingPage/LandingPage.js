4e3964ce87bf66147873090820154bcc45693f2e
Jean <jeanjyjh@gmail.com>
2024-10-29 22:21:03 +1300

    Add a product loading spinner on landing page

--- src/page/LandingPage/LandingPage.js
+++ src/page/LandingPage/LandingPage.js
@@ -1,6 +1,6 @@
 import React, { useEffect } from "react";
 import ProductCard from "./components/ProductCard";
-import { Row, Col, Container } from "react-bootstrap";
+import { Row, Col, Container, Spinner } from "react-bootstrap";
 import { useSearchParams } from "react-router-dom";
 import { useDispatch, useSelector } from "react-redux";
 import { getProductList } from "../../features/product/productSlice";
@@ -8,16 +8,25 @@ import { getProductList } from "../../features/product/productSlice";
 const LandingPage = () => {
   const dispatch = useDispatch();
 
-  const productList = useSelector((state) => state.product.productList);
+  // const productList = useSelector((state) => state.product.productList);
+  // const loading = useSelector((state) => state.product);
+  const { productList, loading: productLoading } = useSelector(
+    (state) => state.product
+  );
   const [query] = useSearchParams();
   const name = query.get("name");
+
   useEffect(() => {
-    dispatch(
-      getProductList({
-        name,
-      })
-    );
+    dispatch(getProductList({ name }));
   }, [query]);
+
+  if (productLoading) {
+    return (
+      <div className="d-flex justify-content-center align-items-center vh-100">
+        <Spinner animation="border" />
+      </div>
+    );
+  }
 
   return (
     <Container>
